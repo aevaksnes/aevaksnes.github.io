d4ba435bc44af7c20bff81081a1b08a0d1882a27
Eva Aksnes <aevaksnes@hotmail.com>
2026-04-07 09:32:17 +0200

    Configure next.js for static export

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // To enable static export for GitHub Pages
+  images: {
+    unoptimized: true, // GitHub Pages does not support dynamic image optimization, so we disable it
+  },
 };
 
 export default nextConfig;
